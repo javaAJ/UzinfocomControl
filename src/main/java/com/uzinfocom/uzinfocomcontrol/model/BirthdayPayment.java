00439b72e07d7c9cd61229c80a4e0efee3d5ad06
Jahongir <jahongirakbarov007@gmail.com>
2025-07-09 00:31:16 +0500

    fix problem

--- src/main/java/com/uzinfocom/uzinfocomcontrol/model/BirthdayPayment.java
+++ src/main/java/com/uzinfocom/uzinfocomcontrol/model/BirthdayPayment.java
@@ -18,7 +18,7 @@ public class BirthdayPayment {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @ManyToOne()
+    @ManyToOne(fetch =  FetchType.EAGER)
     private User user;
     private Double paymentAmount;
     private Double amountPaid;
