e93830c56106429650f2f3917dad1744b95baac5
Jahongir <jahongirakbarov007@gmail.com>
2025-07-09 00:24:02 +0500

    fix problem

--- src/main/java/com/uzinfocom/uzinfocomcontrol/model/repository/UserBirthdayRepository.java
+++ src/main/java/com/uzinfocom/uzinfocomcontrol/model/repository/UserBirthdayRepository.java
@@ -3,10 +3,16 @@ package com.uzinfocom.uzinfocomcontrol.model.repository;
 import com.uzinfocom.uzinfocomcontrol.model.BirthdayPayment;
 import com.uzinfocom.uzinfocomcontrol.model.User;
 import com.uzinfocom.uzinfocomcontrol.model.UserBirthday;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface UserBirthdayRepository extends JpaRepository<UserBirthday, Long> {
+    @EntityGraph(attributePaths = "usersBirthdayPayment")
+    List<UserBirthday> findAll();
+
     UserBirthday findByUserBirthdayId(Long id);
 }
